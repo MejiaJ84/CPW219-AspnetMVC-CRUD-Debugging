2278e257453f1510038ca8bdb99c8d2bf721e80f
MejiaJ84 <jtmejia1984@gmail.com>
2022-02-21 19:24:31 -0800

    Added to the connection string

--- CPW219-AspnetMVC-CRUD-Debugging/Program.cs
+++ CPW219-AspnetMVC-CRUD-Debugging/Program.cs
@@ -4,12 +4,14 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-
 string dbConnection = builder.Configuration.GetConnectionString("CoreWithBugsDB");
 builder.Services.AddDbContext<ProductContext>(
         options => options.UseSqlServer(dbConnection)
     );
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
